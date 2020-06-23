6cfa29ef57cc2d169d9332704399de6edbebbd3e
Ian Kirker <ian.kirker@gmail.com>
2020-06-23 18:21:06 +0100

    Adds logging to glob expansion

--- internal/keyscan/globslice.go
+++ internal/keyscan/globslice.go
@@ -13,9 +13,12 @@ import (
 //  --> https://golang.org/pkg/path/filepath/#Glob
 // Note that we're also absoluting and 'cleaning' them, because relative paths are kind of useless in output.
 func GetPathsByGlob(g []string) ([]string, error) {
+	log.WithFields(log.Fields{"num_globs": len(g)}).Info("Expanding globs")
 	paths := make([]string, 0)
 	for _, v := range g {
+		log.WithFields(log.Fields{"glob": v}).Debug("Expanding glob")
 		matches, err := filepath.Glob(v)
+		log.WithFields(log.Fields{"matches": len(matches)}).Debug("Expanded")
 		if err != nil {
 			log.Error(err)
 		}
@@ -30,5 +33,6 @@ func GetPathsByGlob(g []string) ([]string, error) {
 			paths = append(paths, m)
 		}
 	}
+	log.WithFields(log.Fields{"num_matches": len(paths)}).Info("Expansion complete")
 	return paths, nil
 }
